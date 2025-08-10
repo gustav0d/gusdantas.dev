365ced4590e80946faefec8991a10014172bf4c6
Gustavo Dantas <gusdantas15@gmail.com>
2025-08-10 00:17:05 -0300

    feat(gusdantas.dev): move to gusdantas.dev

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
   title: "Dantas' blog",
-  website: 'https://gustav0d.github.io',
+  website: 'https://gusdantas.dev',
   description: 'The place where I share some of my ideas and thoughts',
   author: 'Gustavo Dantas <gusdantas15@gmail.com>',
 };
 
 export const GITHUB_PROFILE_URL = 'https://github.com/gustav0d';
-export const GITHUB_REPO_URL = `${GITHUB_PROFILE_URL}/gustav0d.github.io`;
+export const GITHUB_REPO_URL = `${GITHUB_PROFILE_URL}/gusdantas.dev`;
 export const TWITTER_URL = 'https://twitter.com/gusdantas';
